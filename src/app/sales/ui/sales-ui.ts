468b37a5eebd042527eff84e245bc8972ff518c6
Andres Carrillo <andresjoe24@gmail.com>
2025-08-11 23:42:49 -0500

    fix: delete a non used view child

--- src/app/sales/ui/sales-ui.ts
+++ src/app/sales/ui/sales-ui.ts
@@ -1,14 +1,7 @@
-import {
-  Component,
-  signal,
-  ViewChild,
-  AfterViewInit,
-  inject,
-} from '@angular/core';
+import { Component, signal, AfterViewInit, inject } from '@angular/core';
 import { CardMetric } from './components/shared/card-metric/card-metric';
 import { SalesOverview } from './components/sales-overview/sales-overview';
 import { SalesByRegion } from './components/sales-by-region/sales-by-region';
-import { DonutChart } from './components/shared/donut-chart/donut-chart';
 import { RegisteredUsers } from './components/registered-users/registered-users';
 import { ListIntegration } from './components/list-integration/list-integration';
 import { GetStatsUseCase } from '../application/use-case/get-stats.usecase';
@@ -27,25 +20,13 @@ import { StatsResponse } from '../domain/interfaces/stats.interface';
   styleUrl: './sales-ui.scss',
 })
 export class SalesUi implements AfterViewInit {
-  @ViewChild('trafficChart') trafficChart!: DonutChart;
-
   private readonly getStatsUseCase = inject(GetStatsUseCase);
   protected readonly stats = signal<StatsResponse[]>([]);
 
   ngAfterViewInit(): void {
     this.getStatsUseCase.execute().subscribe({
-      next: (stats) => {
-        if (Array.isArray(stats)) {
-          this.stats.set(stats);
-        } else {
-          console.error('Stats response is not an array:', stats);
-          this.stats.set([]);
-        }
-      },
-      error: (error) => {
-        console.error('Error fetching stats:', error);
-        this.stats.set([]);
-      },
+      next: (stats) => this.stats.set(stats),
+      error: (error) => console.error(error),
     });
   }
 }
